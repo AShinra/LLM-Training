dde3bf4782fb8ff43be864bd6072220c414bd6cf
Athan <jonpuray@gmail.com>
2025-01-09 00:25:50 +0800

    added file uploader

--- main.py
+++ main.py
@@ -38,18 +38,16 @@ if uploaded_file is not None:
             if st.button('Submit'):
                 for i in df.index:
                     content_to_summarize = df.loc[i, selected_column]
-                    st.write(content_to_summarize)
 
-
-    # # # Creating a form in the Streamlit app for user input
-    # with st.form('my_form'):
-    #     # Adding a text area for user input with a default prompt
-    #     text = st.text_area('Enter text:', '')
-    #     # Adding a submit button for the form
-    #     submitted = st.form_submit_button('Submit')
-    #     # Displaying a warning if the entered API key does not start with 'sk-'
-    #     if not openai_api_key.startswith('sk-'):
-    #         st.warning('Please enter your OpenAI API key!', icon='⚠')
-    #     # If the form is submitted and the API key is valid, generate a response
-    #     if submitted and openai_api_key.startswith('sk-'):
-    #         generate_response(f'{api_prompt()}\n\n{text}')
+                    # Creating a form in the Streamlit app for user input
+                    with st.form('my_form'):
+                        # Adding a text area for user input with a default prompt
+                        text = st.text_area('Enter text:', '')
+                        # Adding a submit button for the form
+                        submitted = st.form_submit_button('Submit')
+                        # Displaying a warning if the entered API key does not start with 'sk-'
+                        if not openai_api_key.startswith('sk-'):
+                            st.warning('Please enter your OpenAI API key!', icon='⚠')
+                        # If the form is submitted and the API key is valid, generate a response
+                        if submitted and openai_api_key.startswith('sk-'):
+                            generate_response(f'{api_prompt()}\n\n{content_to_summarize}')
