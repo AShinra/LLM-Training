ae42ff7e6f76251c61837dd7d34f0024a4da9b0d
Athan <jonpuray@gmail.com>
2025-01-08 23:51:29 +0800

    added file uploader

--- pre_process.py
+++ pre_process.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def get_headers(uploaded_file):
 
-    df = pd.read_excel(uploaded_file)
+    df = pd.read_excel(uploaded_file, index_col=False)
 
     headers = df.columns
     
