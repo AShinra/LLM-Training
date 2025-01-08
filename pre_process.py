0041dbd9b1ca68dd5af7b22850a27c248fbb4c42
Athan <jonpuray@gmail.com>
2025-01-08 23:53:41 +0800

    added file uploader

--- pre_process.py
+++ pre_process.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import streamlit as st
 
 
 def get_headers(uploaded_file):
 
     df = pd.read_excel(uploaded_file)
+    st.dataframe(df)
     df.reset_index(drop=True, inplace=True)
 
     headers = df.columns
